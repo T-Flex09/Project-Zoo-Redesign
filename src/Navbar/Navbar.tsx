46dd25c49d134fe4afc8b1696fe899c25b72859c
T-Flex09 <filiptiron0@gmail.com>
2026-03-31 23:39:26 +0300

    ts pmo

--- src/Navbar/Navbar.tsx
+++ src/Navbar/Navbar.tsx
@@ -1,17 +1,20 @@
 import styles from './Navbar.module.css';
 import Logo from '/src/assets/Sigla_mov.png';
-// import React, {useEffect} from 'react';
+import React, {useState} from 'react';
 
 export default function Navbar() {
 
+    let [themeButtonIcon, setThemeButtonIcon] = useState('✦');
+
     function toggleTheme() {
         document.documentElement.classList.toggle('light-mode');
+        setThemeButtonIcon(themeButtonIcon == '✦' ? '⏾' : '✦');
     }
 
     return (
         <nav>
             <img className={styles.logo} src={Logo}></img>
-            <button className={styles.themeSwitcher} onClick={toggleTheme}>test</button>
+            <button className={styles.themeSwitcher} onClick={toggleTheme}>{themeButtonIcon}</button>
         </nav>
     );
 }

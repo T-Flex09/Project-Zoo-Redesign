512313decfa4e9768a999563f4f07d35323880da
T-Flex09 <filiptiron0@gmail.com>
2026-03-31 16:18:17 +0300

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import Navbar from './Navbar/Navbar.tsx';
+import Footer from './Footer/Footer.tsx';
+
+function App() {
+
+    return (
+        <>
+            <Navbar />
+            <Footer />
+        </>
+    );
+}
+
+export default App;

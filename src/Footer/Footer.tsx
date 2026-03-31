96ec354125a3aa281d0d81c7ee7e5b2ffb3991b5
T-Flex09 <filiptiron0@gmail.com>
2026-03-31 16:54:00 +0300

    grr vercel

--- src/Footer/Footer.tsx
+++ src/Footer/Footer.tsx
@@ -1,6 +1,10 @@
 import styles from './Footer.module.css';
 
 export default function Footer() {
+    // ca sa nu faca urat deocamdata vercel
+    const style = styles;
+    console.log(style);
+    //
     return (
         <footer></footer>
     );
--- src/Navbar/Navbar.tsx
+++ src/Navbar/Navbar.tsx
@@ -1,6 +1,10 @@
 import styles from './Navbar.module.css';
 
 export default function Navbar() {
+    // ca sa nu faca urat deocamdata vercel
+    const style = styles;
+    console.log(style);
+    //
     return (
         <nav></nav>
     );

9a0a3d9ad5be653e52e68eda981779ca71f26eee
GalaXy11232 <neamtumatei08@gmail.com>
2026-04-02 00:16:41 +0300

    hexagon grid background animation
    o sa aiba nevoie de niste imbunatatiri la culoare si la aspect in general dar baza functioneaza 😛

--- src/Pages/Home/Home.tsx
+++ src/Pages/Home/Home.tsx
@@ -1,7 +1,17 @@
-// import styles from './Home.module.css'; 
+import './Home.css'; 
+import { useEffect } from 'react';
+import generateHexagonGrid from './hexgrid';
 
 export default function Home() {
+    useEffect(() => {
+        generateHexagonGrid();
+    }, []);
+
     return (<>
         <h1>Welcome back home nigga ❤️</h1>
+
+        <div className="background-underlay">
+            <div className="hex-grid"> </div>
+        </div>
     </>);
 }

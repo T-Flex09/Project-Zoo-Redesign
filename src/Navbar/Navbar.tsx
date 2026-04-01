4625b529232f01363599d0bb0b292de98acdba1e
T-Flex09 <filiptiron0@gmail.com>
2026-04-01 21:23:25 +0300

    update navbar module

--- src/Navbar/Navbar.tsx
+++ src/Navbar/Navbar.tsx
@@ -2,7 +2,7 @@ import styles from './Navbar.module.css';
 import Logo from '/src/assets/Sigla_mov.png';
 import {useState} from 'react';
 
-type PageType = 'home' | 'sponsors';
+type PageType = 'home' | 'team' | 'activity' | 'results' | 'sponsors' | 'gallery';
 
 interface NavbarProps {
   setPage: (page: PageType) => void;
@@ -10,19 +10,24 @@ interface NavbarProps {
 
 export default function Navbar({setPage}: NavbarProps) {
 
-    let [themeButtonIcon, setThemeButtonIcon] = useState('✦');
+    let [themeButtonIcon, setThemeButtonIcon] = useState('☀️');
 
     function toggleTheme() {
         document.documentElement.classList.toggle('light-mode');
-        setThemeButtonIcon(themeButtonIcon == '✦' ? '⏾' : '✦');
+        setThemeButtonIcon(themeButtonIcon == '☀️' ? '🌙' : '☀️');
     }
 
     return (
         <nav>
             <img className={styles.logo} src={Logo} onClick={() => setPage('home')}></img>
 
+            <div className='linksContainer'></div>
             <button onClick={() => setPage('home')}>Home</button>
+            <button onClick={() => setPage('team')}>Our Team</button>
+            <button onClick={() => setPage('activity')}>Activity</button>
+            <button onClick={() => setPage('results')}>Results</button>
             <button onClick={() => setPage('sponsors')}>Sponsors</button>
+            <button onClick={() => setPage('gallery')}>Gallery</button>
 
             <button className={styles.themeSwitcher} onClick={toggleTheme}>{themeButtonIcon}</button>
         </nav>

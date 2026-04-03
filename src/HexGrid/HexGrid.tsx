e583fb60b0bf55941ba3a92773bc48510beed1c3
T-Flex09 <filiptiron0@gmail.com>
2026-04-03 23:54:26 +0300

    holy vibe code

---
+++ src/HexGrid/HexGrid.tsx
@@ -0,0 +1,55 @@
+import { useEffect, useState } from "react";
+import styles from "./Hexgrid.module.css";
+
+export default function Hexgrid() {
+    const [hexList, setHexList] = useState<any[]>([]);
+
+    function configureHexList() {
+        let newHexList: any[] = [];
+
+        let size_scale = Math.max(window.innerWidth / 1536, window.innerHeight / 864);
+        const hexWidth = 100 * size_scale;
+        const hexHeight = 85 * size_scale;
+
+        const vertSpacing = hexHeight + 6;
+        const horizSpacing = hexWidth + 10;
+
+        const cols = Math.ceil(window.innerWidth / horizSpacing) + 1;
+        const rows = Math.ceil(window.innerHeight / vertSpacing) + 1;
+
+        for (let r = 0; r < 1.1 * rows; r++) {
+            for (let c = 0; c < 1.5 * cols; c++) {
+
+                newHexList.push({
+                    width: hexWidth,
+                    height: hexHeight,
+                    left: c * (horizSpacing - 30 * (size_scale * (size_scale <= 2 ? 1 : 0.9))),
+                    top: r * vertSpacing + (c % 2 === 0 ? 1 : -1) * vertSpacing / 4.0,
+                    delay: (2 * r * rows + c) * 0.0075
+                });
+            }
+        }
+
+        setHexList(newHexList);
+    }
+
+    useEffect(() => {
+        configureHexList();
+    }, []);
+
+    return (<>
+        <div className={styles.backgroundUnderlay}>
+            <div className={styles.hexGrid}>
+                {hexList.map((hex, i) => (
+                    <div key={i} style={{
+                        width: `${hex.width}px`,
+                        height: `${hex.height}px`,
+                        left: `${hex.left}px`,
+                        top: `${hex.top}px`,
+                        animationDelay: `${hex.delay}s`
+                    }}></div>
+                ))}
+            </div>
+        </div>
+    </>);
+}
--- src/Pages/Home/Home.tsx
+++ src/Pages/Home/Home.tsx
@@ -1,8 +1,7 @@
-// import { useEffect } from "react";
-import HexGrid from "../../HexGrid Background/HexGrid.tsx";
-// import generateHexagonGrid from "../../HexGrid Background/hexgrid.ts";
+import HexGrid from "../../HexGrid/HexGrid";
 
 export default function Home() {
+
     return (<>
         <h1>Welcome back home nigga ❤️</h1>
         <HexGrid />
